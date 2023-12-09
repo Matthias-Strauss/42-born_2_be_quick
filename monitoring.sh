b56b53f617941d04937259b350f1201681c4ee4c
MSTR <139340565+Matthias-Strauss@users.noreply.github.com>
2023-12-09 23:08:18 +0100

    Add files via upload

--- monitoring.sh
+++ monitoring.sh
@@ -2,9 +2,7 @@
 #   ___  ____  ____  ____  ____  ___  _  _ 
 #  / _ \(___ \(  __)(  __)(_  _)/ __)/ )( \
 # (__  ( / __/ ) _)  ) _)   )( ( (__ ) __ (
-#   (__/(____)(__)  (____) (__) \___)\_)(_/
-
-display_info() {
+#   (__/(____)(__)  (____) (__) \___)\_)(_/.by mstrauss
 
     # COLORS
     COLOR_TITLE="\e[1;34m"  # Blue for titles
@@ -66,38 +64,39 @@ ${COLOR_RESET}"
     ###   ########.      
 "
 
-    content="
+content='
 System Information at $(date)
 Operating System & Kernel Version: $(uname -a)
-Physical Processors: $(lscpu | grep 'Socket(s):' | awk '{print $2}')
-Virtual Processors: $(lscpu | grep 'CPU(s):' | head -1 | awk '{print $2}')"
-Available RAM & Utilization: $(free -h | awk '/^Mem/ {print $4 "/" $2 " (" int($3/$2*100) "%)"}')
-echo -e "Available Memory & Utilization: $(free -h | awk '/^Swap/ {print $4 "/" $2 " (" int($3/$2*100) "%)"}')
-Processor Utilization: $(top -bn1 | grep '%Cpu' | awk '{print $2 + $4}')%
-Last Reboot: $(who -b | awk '{print $3, $4}')
+Physical Processors: $(lscpu | grep '\''Socket(s):'\'' | awk '\''{print $2}'\'')
+Virtual Processors: $(lscpu | grep '\''CPU(s):'\'' | head -1 | awk '\''{print $2}'\'')
+Available RAM & Utilization: $(free -h | awk '\''/^Mem/ {print $4 "/" $2 " (" int($3/$2*100) "%)"}'\'')
+Available Memory & Utilization: $(free -h | awk '\''/^Swap/ {print $4 "/" $2 " (" int($3/$2*100) "%)"}'\'')
+Processor Utilization: $(top -bn1 | grep '\''%Cpu'\'' | awk '\''{print $2 + $4}'\'')%
+Last Reboot: $(who -b | awk '\''{print $3, $4}'\'')
 LVM Status: $(sudo lvdisplay > /dev/null 2>&1 && echo -e Active || echo -e Inactive)
 Active Connections: $(netstat -an | grep -c ESTABLISHED)
 Users Logged In: $(who | wc -l)
-IPv4 Address: $(hostname -I | awk '{print $1}')
-MAC Address: $(ip link | awk '/ether/ {print $2;exit}')
-Sudo Commands Executed: $(cat /var/log/auth.log* | grep -c 'COMMAND=.*sudo')
-"
+IPv4 Address: $(hostname -I | awk '\''{print $1}'\'')
+MAC Address: $(ip link | awk '\''/ether/ {print $2;exit}'\'')
+Sudo Commands Executed: $(cat /var/log/auth.log* | grep -c '\''COMMAND=.*sudo'\'')
+'
 
-    # Clear console
-    clear
 
-    # Calculate content length for box sizing
-    content_length=$(echo "${content}" | wc -L)
+    # # Clear console
+    # clear
 
-    # Determine the number of lines in ASCII art
-    ascii_lines=$(echo -n "${logo}" | wc -l)
+    # # Calculate content length for box sizing
+    # content_length=$(echo "${content}" | wc -L)
 
-    # Print ASCII art and content with the box outline
-    for ((i = 1; i <= $ascii_lines; i++)); do
-        printf '%s' "$(echo "${logo}" | sed -n "${i}p")"
-        printf '%*s' $((content_length + 5 - ${#logo})) "$(echo "${content}" | sed -n "${i}p")"
-        printf '\n'
-    done
+    # # Determine the number of lines in ASCII art
+    # ascii_lines=$(echo -n "${logo3}" | wc -l)
+
+    # # Print ASCII art and content with the box outline
+    # for ((i = 1; i <= $ascii_lines; i++)); do
+    #     printf '%s' "$(echo "${logo3}" | sed -n "${i}p")"
+    #     printf '%*s' $((content_length + 5 - ${#logo3})) "$(echo "${content}" | sed -n "${i}p")"
+    #     printf '\n'
+    # done
 
 
 
@@ -116,33 +115,3 @@ Sudo Commands Executed: $(cat /var/log/auth.log* | grep -c 'COMMAND=.*sudo')
     # # Print the bottom of the box
     # printf '╚═%.0s' $(seq 1 "${content_length}")
     # printf '╝\n'
-}
-
-
-while true
-do
-    display_info | wall
-    sleep 600 # 10 minutes
-done
-
-# ╔
-
-# ╦	
-
-# ╣
-
-# ╠
-
-# ╩
-
-# ╬	
-
-# ═
-
-# ╚
-
-# ╝
-
-# ╗
-
-# ║
